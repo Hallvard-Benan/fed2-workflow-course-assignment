f89225fb5685ff7ae48ada1c073b2b83fdae3778
Mikaelajoelsen <111288566+Mikaelajoelsen@users.noreply.github.com>
2023-11-09 13:41:55 +0100

    changed email in login

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    expect(true).to.equal(true);
+describe("user can login and access the profile", () => {
+  it("profile", () => {
+    cy.visit("https://develop-sindri.netlify.app/");
   });
 });
